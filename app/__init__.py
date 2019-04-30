e0adb5c85e34ad8ecfd9a21842ee65db882f2941
Craig Lou <craiglou1@gmail.com>
2019-04-30 14:51:11 -0400

    Form pages complete, TODO: contact page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 #setup imports
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
 
 #setup app variables
 app = Flask(__name__)
 boostrap = Bootstrap(app)
+app.config.from_object(Config)
 
 #go to routes
 from app import routes
