e3f5a07bb8a5842ef291e634ce43873f0a3f1b0d
Craig Lou <craiglou1@gmail.com>
2019-05-08 13:27:03 -0400

    heroku postgresql ready

--- config.py
+++ config.py
@@ -8,4 +8,8 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # set the uri for the database location, this db will be locally stored through a sqlite database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+
+
+# use this when setting up postgres
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:nablimeseL1!@localhost:5432/twitter'
